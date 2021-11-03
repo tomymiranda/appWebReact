6e17a82dbd0dfad02ba944b5dc36deb9c1435af3
unknown <tomasmiranda98@hotmail.com>
2021-11-02 21:01:27 -0300

    se agrega funcionalidad de navegar

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,6 +1,7 @@
 import React, {useState,useEffect} from 'react';
 import { Navbar } from "./Components/Navbar";
 import  { Personajes } from "./Components/Personajes";
+import { Pagination } from "./Components/Pagination";
 
 
 function App() {
@@ -9,23 +10,37 @@ function App() {
 
   
   const [personajesState,setStatePersonajes] = useState ([])
+  const [info,setStateInfo] = useState([])
   
   
   const personajesObtenidosDeApi = (url) => fetch(url)
                                     .then(response => response.json())
-                                    .then(data => setStatePersonajes(data.results))
+                                    .then(data => {
+                                      setStatePersonajes(data.results);
+                                      setStateInfo(data.info);
+                                    })
                                     .catch(error => console.log(error))
 
   
   useEffect(() => {
     personajesObtenidosDeApi(urlDeApi)
   },[])
+
+  const mostrarPrevio = () => {
+    personajesObtenidosDeApi(info.prev)
+  }
+
+  const mostrarSiguiente = () => {
+    personajesObtenidosDeApi(info.next)
+  }
   
 return (
     <>
     <Navbar titlePage = {"Rick y Morty App"}/>
-    <div className ="container" >
+    <div className ="container mt-5" >
+      <Pagination hayPrevio = {info.prev}  haySiguiente = {info.next} previo = {mostrarPrevio} siguiente = {mostrarSiguiente} />
       <Personajes personajesARepresentar = {personajesState} />
+      <Pagination hayPrevio = {info.prev}  haySiguiente = {info.next} previo = {mostrarPrevio} siguiente = {mostrarSiguiente}/>
     </div>
     </>
   );
--- web-app/src/Components/Personajes.jsx
+++ web-app/src/Components/Personajes.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Tarjeta.css";
 
-export const Personajes = ({ personajesARepresentar }) => {
+export const Personajes = ({ personajesARepresentar = [] }) => {
   return (
     <div className="row">
-      {personajesARepresentar.map((per, idx) => (
-        <div key={idx} className="col">
-          <div className="card anchoMinimo" >
-            <img src={per.image} class="card-img-top" />
+      {personajesARepresentar.map((item, idx) => (
+        <div key={idx} className="col-md-4 mb-4">
+          <div className="card">
+            <img src={item.image} />
+            <div className="card-body">
+              <h5 className="card-title">{item.name}</h5>
+              <hr />
+              <p>Especie: {item.species}</p>
+              <p>Lugar: {item.location.name}</p>
+            </div>
           </div>
         </div>
       ))}
